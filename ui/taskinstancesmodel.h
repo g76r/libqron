70b89c1b4bda9b88ddb9e8ed300eaa0e95013044
Grégoire Barbier <devel@g76r.eu>
2016-11-08 22:32:06 +0100

    ui: removed unused member in TaskInstancesModel

--- ui/taskinstancesmodel.h
+++ ui/taskinstancesmodel.h
@@ -1,4 +1,4 @@
-/* Copyright 2013-2015 Hallowyn and others.
+/* Copyright 2013-2016 Hallowyn and others.
  * This file is part of qron, see <http://qron.eu/>.
  * Qron is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -27,7 +27,6 @@
 class LIBQRONSHARED_EXPORT TaskInstancesModel : public SharedUiItemsTableModel {
   Q_OBJECT
   Q_DISABLE_COPY(TaskInstancesModel)
-  QList<TaskInstance> _instances;
   bool _keepFinished;
   QString _customActions;
 
